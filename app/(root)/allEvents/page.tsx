5a83c6b6c78994ba490b13ccbcfa1fc5981297b9
Femi <osibemekunfemi@gmail.com>
2024-08-08 18:48:19 +0100

    added events route

--- app/(root)/allEvents/page.tsx
+++ app/(root)/allEvents/page.tsx
@@ -1,8 +1,37 @@
+import CatergoryFilter from "@/components/shared/CatergoryFilter";
+import Collection from "@/components/shared/Collection";
+import Search from "@/components/shared/Search";
+import { getAllEvents } from "@/lib/actions/event.actions";
+import { SearchParamProps } from "@/types";
 
-const AllEvents = () => {
+export default async function AllEvents({ searchParams }: SearchParamProps) {
+  const page = Number(searchParams?.page) || 1;
+  const searchText = (searchParams?.query as string) || '';
+  const category = (searchParams?.category as string) || '';
+  const events = await getAllEvents({
+    query: searchText,
+    category,
+    page,
+    limit: 6
+  });
   return (
-    <div>AllEvents</div>
+    <div>
+      <section id="events" className="wrapper my-8 w-full flex flex-col gap-8 md:gap-12">
+        <div className="flex w-full flex-row gap-5 md:flex-row">
+          <Search />
+          <CatergoryFilter />
+        </div>
+        <Collection 
+          data={events?.data}
+          emptyTitle="No Events Found"
+          emptyStateSubtext="Come back later"
+          collectionType="All_Events"
+          limit={6}
+          page={page}
+          totalPages={events?.totalPages}
+        />
+      </section>
+    </div>
   )
 }
 
-export default AllEvents
